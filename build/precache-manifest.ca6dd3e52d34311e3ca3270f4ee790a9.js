efcd374ffd0aedc4945471cf8e1ab406bfb8a338
brjoaof <brjoaof@gmail.com>
2020-09-24 00:57:18 -0300

    deploy heroku

---
+++ build/precache-manifest.ca6dd3e52d34311e3ca3270f4ee790a9.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffe952d3562e773ddff0a064b8464782",
+    "url": "/index.html"
+  },
+  {
+    "revision": "14938e717d6f68762633",
+    "url": "/static/css/main.bf477412.chunk.css"
+  },
+  {
+    "revision": "59f2dd35b967c3d5c2d5",
+    "url": "/static/js/2.0a5954af.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/static/js/2.0a5954af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "14938e717d6f68762633",
+    "url": "/static/js/main.1b0dc358.chunk.js"
+  },
+  {
+    "revision": "722677a823a403934f8e",
+    "url": "/static/js/runtime-main.92fae014.js"
+  },
+  {
+    "revision": "e74285672c560f6dc6480129b15d963b",
+    "url": "/static/media/11333.e7428567.jpg"
+  },
+  {
+    "revision": "be0141bac1cc1cb7a04df4432cf95878",
+    "url": "/static/media/TMJ.be0141ba.png"
+  },
+  {
+    "revision": "48a37f89bdab154fe4fcff9492c4fe39",
+    "url": "/static/media/foto.48a37f89.jpg"
+  },
+  {
+    "revision": "3986758d822c47da5fd448e17d22bab2",
+    "url": "/static/media/jhonatan.3986758d.png"
+  },
+  {
+    "revision": "60bb28c170afe04bc0d8db30933a5c4c",
+    "url": "/static/media/joao.60bb28c1.png"
+  },
+  {
+    "revision": "9735fb84c20a2f1bede25d5a135f4cfc",
+    "url": "/static/media/marcelo.9735fb84.png"
+  },
+  {
+    "revision": "39eb239b0511d742ebedf2c6afc888cb",
+    "url": "/static/media/mariana.39eb239b.png"
+  },
+  {
+    "revision": "bb1d30e58eb91d363a8993148fd67df1",
+    "url": "/static/media/tatiane.bb1d30e5.png"
+  }
+]);
